5d5ebc683f20f81181c9388fa7bb4b628ed64252
0xFN89 <eighteen089@gmail.com>
2016-01-18 00:46:04 +0530

    created a local_settings

--- simblog/settings.py
+++ simblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+from simblog import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blogbase',
         'USER':'root',
-        'PASSWORD':'rohit123',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
     }
 }
