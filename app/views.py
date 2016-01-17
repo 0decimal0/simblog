b3d74f2ef6284290ebb3fafd3dcd2e233844157e
0xFN89 <eighteen089@gmail.com>
2016-01-18 01:24:00 +0530

    Added view

--- app/views.py
+++ app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+        return render_to_response('index.html',{
+            'categories':Category.objects.all(),
+            'posts':Blog.objects.all()[:5]
+            },context_instance=RequestContext(request))
 
+def view_post(request,slug):
+    return render_to_response('view_post.html',{
+        'post':get_object_or_404(Blog,slug = slug)
+        },context_instance=RequestContext(request))
+
+def view_category(request,slug):
+    category = get_object_or_404(Category,slug = slug)
+    return render_to_response('view_category.html',{
+        'category':category,
+        'posts':Blog.objects.filter(category=category)[:5]
+        })
